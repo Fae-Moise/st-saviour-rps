4ae4f3a84d2653ed5b910b2e8955e03e91699b5c
fmoise <fmoise28@stsaviour.org>
2026-03-17 08:30:34 -0400

    better rock emoji

--- rps.py
+++ rps.py
@@ -9,3 +9,8 @@ def print_dramatic_text(text: str, delay=0.1):
 
 if __name__ == '__main__':
     print_dramatic_text('welcome to rock, paper, scissors! 🗿📃✂️')
+options = ('rock' , 'scissors' , 'paper' )
+choice = random.randint (0, 2 )
+
+
+print ('computer player selected: ' + options[choice]) 
