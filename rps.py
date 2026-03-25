24ba650ab75fa54d911a5f5ec8e96a582978add5
fmoise <fmoise28@stsaviour.org>
2026-03-25 08:32:56 -0400

    better rock emoji

--- rps.py
+++ rps.py
@@ -9,8 +9,37 @@ def print_dramatic_text(text: str, delay=0.1):
 
 if __name__ == '__main__':
     print_dramatic_text('welcome to rock, paper, scissors! 🗿📃✂️')
-options = ('rock' , 'scissors' , 'paper' )
-choice = random.randint (0, 2 )
+    options = ('rock' , 'scissors' , 'paper' )
+    choice = random.randint (0, 2 )
 
 
-print ('computer player selected: ' + options[choice]) 
+    print ('computer player selected: ' + options[choice])
+    computer = options[choice]
+
+    response = input('please choose rock, paper, or scissors:')
+
+    while response != 'rock' and response !='scissors' and response != 'paper':
+        response = input ('please choose rock,paper, or scissors: ')
+
+    print(f'player selected{response}')
+
+    if response == 'rock' and computer == 'rock':
+        print('you tie!')
+    if response == 'rock' and computer  == 'scissors':
+        print('you win!')
+    if response == 'rock' and computer == 'paper':
+        print('you lose!')
+    
+    if response == 'scissors' and computer == 'scissors':
+        print('you tie!')
+        if response == 'scissors' and computer == 'paper':
+            print('you win!')
+            if response == 'paper' and computer == 'scissors':
+                print('you lose!')
+
+                if response == 'paper' and computer == 'paper':
+                    print('you tie!')
+                    if response == 'paper' and computer == 'rock':
+                        print('you win!')
+                    if response == 'paper' and computer == 'scissors':
+                        print ('you lose!')
