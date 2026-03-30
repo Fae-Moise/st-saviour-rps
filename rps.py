b85835e69a6b920e35bf6fc306bbec1e99550cc9
fmoise <fmoise28@stsaviour.org>
2026-03-30 13:30:39 -0400

    better rock emoji

--- rps.py
+++ rps.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     response = input('please choose rock, paper, or scissors:')
 
     while response != 'rock' and response !='scissors' and response != 'paper':
-        response = input ('please choose rock,paper, or scissors: ')
+        response = input ('choose rock,paper, or scissors: ')
 
     print(f'player selected{response}')
 
@@ -34,10 +34,10 @@ if __name__ == '__main__':
         print('you tie!')
         if response == 'scissors' and computer == 'paper':
             print('you win!')
-            if response == 'paper' and computer == 'scissors':
-                print('you lose!')
-
-                if response == 'paper' and computer == 'paper':
+        if response == 'paper' and computer == 'scissors':
+            print('you lose!')
+                
+            if response == 'paper' and computer == 'paper':
                     print('you tie!')
                     if response == 'paper' and computer == 'rock':
                         print('you win!')
